e4054628a712dcb68870b5b5baf5366106df9401
abul <abulhasan7bd.gmail.com>
2024-01-13 21:05:32 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+const App = () => {
+  const name = [{ id: Math.floor(Math.random() * 1000), name: "abul" }];
+  const [item, setItem] = useState(name);
+  const [value, setValue] = useState();
+  const [edit, setEdit] = useState(false);
+  const [updateId, setUpdateId] = useState("");
+  useEffect(() => {
+    AOS.init();
+  }, []);
 
-function App() {
+  // insert Data
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!value) {
+      alert("plese something write here.....");
+    } else {
+      setItem((prev) => {
+        return [...prev, { id: Math.floor(Math.random() * 1000), name: value }];
+      });
+      setValue("");
+    }
+  };
+  // delet Data
+  const handleDelet = (id) => {
+    setItem((prev) => {
+      return [...prev].filter((todo) => {
+        return todo.id !== id;
+      });
+    });
+  };
+
+  // Edit Data
+  const EditData = (id) => {
+    let newData = item.find((data) => data.id === id);
+    setEdit(true);
+    setValue(newData.name);
+    setUpdateId(id);
+  };
+
+  // updateData
+  const updateData = (e) => {
+    e.preventDefault();
+    let newData = item.find((data) => data.id === updateId);
+    newData.name = value;
+    setEdit(false);
+    setValue("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {/* todo tittle  */}
+      <h1>Todo App</h1>
+      {/* todo form  */}
+      <form onSubmit={edit ? updateData : handleSubmit}>
+        <input
+          value={value}
+          placeholder="Write your todo...."
+          type="text"
+          onChange={(e) => setValue(e.target.value)}
+        />
+
+        <button type="submit">{edit ? "Update" : "Submit"}</button>
+      </form>
+      {/* todo item  */}
+      <div className="itmes">
+        <span>{item.length}</span>
+        {item.map((item, index) => {
+          return (
+            <div data-aos="flip-right" className="item" key={index}>
+              <h3>{item.name}</h3>
+              <button onClick={() => EditData(item.id)}>Edit</button>
+              <button onClick={() => handleDelet(item.id)}>Delet</button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
